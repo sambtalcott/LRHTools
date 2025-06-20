15f44d842dbb613436b3e20a186e8a09306a362e
Samuel Talcott <stalcott@lrhcares.org>
2025-06-20 10:45:13 -0400

    Added lrh_excel() function

--- R/lrh_csv.R
+++ R/lrh_csv.R
@@ -22,3 +22,54 @@ lrh_csv <- function(x, file, na = "", row.names = FALSE, ...) {
   utils::write.csv(x = x, file = file, na = na, row.names = row.names, ...)
   invisible(x)
 }
+
+#' LRH Excel Writer
+#'
+#' Uses `openxlsx2` to write to an excel file. Accepts a single data frame or a
+#' list of data frames for multiple tabs. Data is written as a table, and column
+#' widths are set to "auto" by default.
+#'
+#' By default, writes to a tempfile and opens the file, but can also be used to
+#' write to specified location with the `file` parameter
+#'
+#' @param x a dataframe or list of dataframes
+#' @param widths column widths (default: auto)
+#' @param table_style Excel table style (default: gray/striped)
+#' @param file path to write to (defaults to tempfile)
+#' @param open open the file after writing? By default, opens tempfiles but not
+#' other files
+#'
+#' @returns the path, invisibly
+#' @export
+#'
+#' @md
+lrh_excel <- function(x, widths = "auto", table_style = "TableStyleMedium1",
+                      file = NULL, open = is.null(file)) {
+
+  force(open)
+  # Get x to be a list of dataframes (if only a single dataframe was provided)
+  if (!is.data.frame(x[[1]])) {
+    nm <- deparse(substitute(x))
+    x <- list(x)
+    names(x) <- nm
+  }
+
+  if (!is.null(names(x))) x <- janitor::clean_names(x)
+
+  wb <- openxlsx2::wb_workbook()
+
+  purrr::iwalk(x, \(df, nm) {
+    wb$
+      add_worksheet(nm)$
+      add_data_table(x = df, table_style = table_style)$
+      set_col_widths(cols = 1:ncol(df), widths = "auto")
+  })
+
+  if (is.null(file)) file <- tempfile(fileext = ".xlsx")
+
+  wb$save(file = file)
+
+  if (open) shell.exec(file)
+
+  invisible(file)
+}
